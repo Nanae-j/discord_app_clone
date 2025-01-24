ebb130de44dde7d0e5f75f1154c24dbd8ea5e2f4
Nanae-j <nanae9577@gmail.com>
2025-01-24 21:32:33 +0900

    [add]scssの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import styles from './page.module.css';
+import styles from './page.module.scss';
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <p className={styles.title}>タイトル</p>
+        <p className={styles.title_sub}>サブタイトル</p>
+      </main>
     </div>
   );
 }
