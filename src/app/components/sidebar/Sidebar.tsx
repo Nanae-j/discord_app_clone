f3bc968e5ec5f20f6c322f13a75d580ebd3b60bf
Nanae-j <nanae9577@gmail.com>
2025-01-29 19:23:47 +0900

    [update,add]sidebarのfooter作成とフォント追加

--- src/app/components/sidebar/Sidebar.tsx
+++ src/app/components/sidebar/Sidebar.tsx
@@ -3,6 +3,9 @@ import './Sidebar.scss';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import AddIcon from '@mui/icons-material/Add';
 import SidebarChannel from './SidebarChannel';
+import MicIcon from '@mui/icons-material/Mic';
+import HeadphonesIcon from '@mui/icons-material/Headphones';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 const Sidebar = () => {
   return (
@@ -46,6 +49,29 @@ const Sidebar = () => {
             <SidebarChannel />
             <SidebarChannel />
             <SidebarChannel />
+          </div>
+
+          <div className="sidebarFooter">
+            <div className="sidebarAccount">
+              <div className="sidebarAccountImageWrap">
+                <Image
+                  src="/bs-logo_icon_black.jpg"
+                  alt=""
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div className="accountName">
+                <h4>nataro</h4>
+                <span>#8162</span>
+              </div>
+            </div>
+
+            <div className="sidebarVoice">
+              <MicIcon />
+              <HeadphonesIcon />
+              <SettingsIcon />
+            </div>
           </div>
         </div>
       </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from 'next';
-// import { Geist, Geist_Mono } from 'next/font/google';
+import { Noto_Sans_JP } from 'next/font/google';
 import './globals.css';
 
-// const geistSans = Geist({
-//   variable: '--font-geist-sans',
-//   subsets: ['latin'],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: '--font-geist-mono',
-//   subsets: ['latin'],
-// });
+const notoSansJP = Noto_Sans_JP({
+  variable: '--font-noto-sans-jp',
+  subsets: ['latin'],
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -24,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="jp">
-      <body className={``}>{children}</body>
+      <body className={notoSansJP.className}>{children}</body>
     </html>
   );
 }
