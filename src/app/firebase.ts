2af3339143f3f01e4ee56cfa9436c7cfa553c492
Nanae-j <nanae9577@gmail.com>
2025-01-31 22:00:21 +0900

    [add]firebaseの初期設定

---
+++ src/app/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore/lite';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDdD9AOSls-S2TRBgY5Qd04WBztY9ZK_rE',
+  authDomain: 'discord-clone-udemy-42978.firebaseapp.com',
+  projectId: 'discord-clone-udemy-42978',
+  storageBucket: 'discord-clone-udemy-42978.firebasestorage.app',
+  messagingSenderId: '1073810906313',
+  appId: '1:1073810906313:web:d97c9e5b964e4ec6d38118',
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { db, auth, provider };
