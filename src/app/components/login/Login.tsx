6420b94973ef41c868dd8d62934648d736bb9ede
Nanae-j <nanae9577@gmail.com>
2025-02-03 22:42:01 +0900

    [update]ログイン機能の実装

--- src/app/components/login/Login.tsx
+++ src/app/components/login/Login.tsx
@@ -1,15 +1,22 @@
 import Image from 'next/image';
 import './Login.scss';
 import { Button } from '@mui/material';
+import { signInWithPopup } from 'firebase/auth';
+import { auth, provider } from '../../firebase';
 
 const Login = () => {
+  const signIn = () => {
+    signInWithPopup(auth, provider).catch((err) => {
+      alert(err.message);
+    });
+  };
   return (
     <div className="login">
       <div className="loginLogo">
-        <Image src="/discordIcon.png" alt="" width={50} height={50} />
+        <Image priority src="/discordIcon.png" alt="" width={50} height={50} />
       </div>
 
-      <Button>ログイン</Button>
+      <Button onClick={signIn}>ログイン</Button>
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,36 @@ import Chat from './components/chat/Chat';
 import Sidebar from './components/sidebar/Sidebar';
 import './page.scss';
 import Login from './components/login/Login';
-import { useAppSelector } from '../lib/hooks';
+import { useAppDispatch, useAppSelector } from '../lib/hooks';
+import { useEffect } from 'react';
+import { auth } from './firebase';
+import { login, logout } from '@/lib/features/userSlice';
 
 export default function Home() {
   const user = useAppSelector((state) => state.user.user);
 
-  console.log(user);
+  // console.log(user);
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((loginUser) => {
+      // console.log(loginUser);
+      if (loginUser) {
+        dispatch(
+          login({
+            uid: loginUser.uid,
+            photo: loginUser.photoURL,
+            email: loginUser.email,
+            displayName: loginUser.displayName,
+          }),
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, [dispatch]);
+
   return (
     <div className="page">
       {user ? (
