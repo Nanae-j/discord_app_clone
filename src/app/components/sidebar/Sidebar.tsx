764438450933e72503aa55451a0a4b5276d44b73
Nanae-j <nanae9577@gmail.com>
2025-02-07 23:12:57 +0900

    カスタムフックスを作成し、リファクタリング

--- src/app/components/sidebar/Sidebar.tsx
+++ src/app/components/sidebar/Sidebar.tsx
@@ -8,39 +8,14 @@ import SidebarChannel from './SidebarChannel';
 import MicIcon from '@mui/icons-material/Mic';
 import HeadphonesIcon from '@mui/icons-material/Headphones';
 import SettingsIcon from '@mui/icons-material/Settings';
-import { auth, db } from '@/app/firebase';
+import { auth } from '@/app/firebase';
 import { useAppSelector } from '@/lib/hooks';
-import { useEffect, useState } from 'react';
-import {
-  collection,
-  query,
-  onSnapshot,
-  DocumentData,
-} from 'firebase/firestore';
-
-interface Channel {
-  id: string;
-  channel: DocumentData;
-}
+import useCollection from '@/hooks/useCollection';
 
 const Sidebar = () => {
-  const [channels, setChannels] = useState<Channel[]>([]);
   const user = useAppSelector((state) => state.user.user);
 
-  const q = query(collection(db, 'channels'));
-
-  useEffect(() => {
-    onSnapshot(q, (querySnapshot) => {
-      const channelsResults: Channel[] = [];
-      querySnapshot.docs.forEach((doc) =>
-        channelsResults.push({
-          id: doc.id,
-          channel: doc.data(),
-        }),
-      );
-      setChannels(channelsResults);
-    });
-  }, []);
+  const { documents: channels } = useCollection('channels');
   return (
     <div className="sidebar">
       {/* SidebarLeft */}
---
+++ src/hooks/useCollection.tsx
@@ -0,0 +1,37 @@
+import { db } from '@/app/firebase';
+import { useEffect, useState } from 'react';
+import {
+  collection,
+  query,
+  onSnapshot,
+  DocumentData,
+  Query,
+} from 'firebase/firestore';
+
+interface Channels {
+  id: string;
+  channel: DocumentData;
+}
+
+const useCollection = (data: string) => {
+  const [documents, setDocuments] = useState<Channels[]>([]);
+
+  const collectionRef: Query<DocumentData> = query(collection(db, data));
+
+  useEffect(() => {
+    onSnapshot(collectionRef, (querySnapshot) => {
+      const channelsResults: Channels[] = [];
+      querySnapshot.docs.forEach((doc) =>
+        channelsResults.push({
+          id: doc.id,
+          channel: doc.data(),
+        }),
+      );
+      setDocuments(channelsResults);
+    });
+  }, []);
+
+  return { documents };
+};
+
+export default useCollection;
