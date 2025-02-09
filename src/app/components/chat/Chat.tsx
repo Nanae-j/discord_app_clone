acb711427c616cfee58ace66e2905aa76de72b69
Nanae-j <nanae9577@gmail.com>
2025-02-09 21:33:58 +0900

    [update]送信したメッセージを書くチャンネルのmessageコレクションに追加

--- src/app/components/chat/Chat.tsx
+++ src/app/components/chat/Chat.tsx
@@ -6,10 +6,45 @@ import GifIcon from '@mui/icons-material/Gif';
 import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 import ChatMessage from './ChatMessage';
 import { useAppSelector } from '@/lib/hooks';
+import { useState } from 'react';
+import {
+  addDoc,
+  collection,
+  CollectionReference,
+  DocumentData,
+  DocumentReference,
+  serverTimestamp,
+} from 'firebase/firestore';
+import { db } from '@/app/firebase';
 
 const Chat = () => {
+  const [inputText, setInputText] = useState<string>('');
+
   const channelName = useAppSelector((state) => state.channel.channelName);
-  console.log(channelName);
+  const channelId = useAppSelector((state) => state.channel.channelId);
+  const user = useAppSelector((state) => state.user.user);
+  // console.log(channelName);
+
+  const sendMessage = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    e.preventDefault();
+    // channelsコレクションの中にあるmessageコレクションの中にメッセージ情報を入れる
+    const collectionRef: CollectionReference<DocumentData, DocumentData> =
+      collection(db, 'channels', String(channelId), 'messages');
+
+    const docRef: DocumentReference<DocumentData> = await addDoc(
+      collectionRef,
+      {
+        message: inputText,
+        timestamp: serverTimestamp(),
+        user: user,
+      },
+    );
+
+    console.log(docRef);
+  };
+
   return (
     <div className="chat">
       {/* chatHeader */}
@@ -22,8 +57,20 @@ const Chat = () => {
       <div className="chatInput">
         <AddCircleOutlineIcon />
         <form action="">
-          <input type="text" placeholder="Udemyへメッセージを送信" />
-          <button type="submit" className="chatInputButton">
+          <input
+            type="text"
+            placeholder="Udemyへメッセージを送信"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setInputText(e.target.value)
+            }
+          />
+          <button
+            type="submit"
+            className="chatInputButton"
+            onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>
+              sendMessage(e)
+            }
+          >
             送信
           </button>
         </form>
