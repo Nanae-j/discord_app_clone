3f42dfb501d0eda3d13b348eafb3763813586b33
Nanae-j <nanae9577@gmail.com>
2025-02-11 09:47:30 +0900

    update

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     domains: [
       'lh3.googleusercontent.com',
       'https://discord-clone-udemy-42978.web.app',
+      'https://discord-clone-udemy-42978.firebaseapp.com/',
     ], // 許可するドメインを追加
   },
   output: 'export',
